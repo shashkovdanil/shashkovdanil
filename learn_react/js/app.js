9c9ae99bbc2a2bb198910b979131d6a84fc1a299
shashkovdanil <shashkovdanil@gmail.com>
2016-09-09 16:01:50 +0500

    react-learning

---
+++ learn_react/js/app.js
@@ -0,0 +1,61 @@
+var my_news = [
+  {
+    author: 'Саша Печкин',
+    text: 'В четчерг, четвертого числа...'
+  },
+  {
+    author: 'Просто Вася',
+    text: 'Считаю, что $ должен стоить 35 рублей!'
+  },
+  {
+    author: 'Гость',
+    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+  }
+];
+
+var News = React.createClass({
+  render: function() {
+    var data = this.props.data;
+    var newsTemplate = data.map(function(item, index) {
+      return (
+        <div key={index}>
+          <p className="news__author">{item.author}:</p>
+          <p className="news__text">{item.text}</p>
+        </div>
+      )
+    })
+		
+    return (
+      <div className="news">
+        {newsTemplate}
+      </div>
+    );
+  }
+});
+
+var Comments = React.createClass({
+  render: function() {
+    return (
+      <div className="comments">
+        Нет новостей - комментировать нечего
+      </div>
+    );
+  }
+});
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="app">
+        Всем привет, я компонент App! Я умею отображать новости.
+        <News data={my_news} />
+        <Comments />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
